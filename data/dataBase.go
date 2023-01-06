c95b9af73de2ee2ce3744455b2e781acff4f0888
Student07 <santoslukombo@gmail.com>
2023-01-06 23:56:20 +0100

    create the dataBase

---
+++ data/dataBase.go
@@ -0,0 +1,60 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func DataBase(email string, password string) {
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE email = ?", email).Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if count > 0 {
+
+		log.Fatal("email adress already exist !")
+		return
+
+	} else {
+		_, err = db.Exec("INSERT INTO users (email, password) VALUES (?, ?)", email, password)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	rows, err := db.Query("SELECT id, email, password FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var id int
+		var email string
+		var password string
+
+		err = rows.Scan(&id, &email, &password)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("id: %v\n", id)
+		fmt.Printf("name: %v\n", email)
+		fmt.Printf("password: %v\n", password)
+
+	}
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	data "Forum/data"
+	"fmt"
+	"log"
+)
 
+func main() {
+	email := identificationEmail()
+	password := identificationPassword()
+	data.DataBase(email, password)
+
+}
+
+func identificationEmail() string {
+	fmt.Println("enter you email:")
+	var email string
+
+	_, err := fmt.Scanln(&email)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return email
+}
+
+func identificationPassword() string {
+	fmt.Println("enter your password:")
+	var password string
+
+	_, err := fmt.Scanln(&password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return password
 }

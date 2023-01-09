756569d6709ea04136f991db22bfc28fba44a2b8
max92 <lmaxla90002@gmail.com>
2023-01-09 19:01:03 +0100

    https

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	data "Forum/data"
 	script "Forum/scripts"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -39,12 +40,33 @@ func main() {
 
 	http.HandleFunc("/error", erreur)
 
-	fmt.Println("Starting server at port: 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
+	// configuration TLS en utilisant les certificats générés
+	config := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
 	}
 
+	// configuration du serveur HTTP
+	server := &http.Server{
+		Addr:         ":8080",
+		TLSConfig:    config,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
+	fmt.Println("Starting server at port: 8080")
+	err := server.ListenAndServeTLS("Key/server.crt", "Key/server.key")
+	if err != nil {
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
